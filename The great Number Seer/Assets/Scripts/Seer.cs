1bf4dc6c4a869aa55e2479679f1b2fb85018a3ee
catalinadg98 <a01411583@itesm.mx>
2018-01-29 08:25:10 -0600

    update

---
+++ The great Number Seer/Assets/Scripts/Seer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Seer : MonoBehaviour {
+
+	private int min;
+	private int max;
+	private int guess;
+	private LevelManager levelManager;
+
+	public int attempts;
+	public Text guessLabel;
+
+	//Initialization
+	void Start (){
+		levelManager = FindObjectOfType<LevelManager>();
+		StartGame();
+		ShowGuess();
+	}
+
+	void Update () {
+
+	}
+
+	void StartGame(){
+		min = 1;
+		max = 1001;
+		//attempts = 0;
+		NextGuess ();
+	}
+
+	public void NextGuess(){
+		guess = Random.Range (min, max);
+		attempts--;
+	}
+		
+	public void ShowGuess(){
+		if (attempts == 0)
+		{
+			levelManager.LoadLevel("Lose");
+		}
+		else
+		{
+			guessLabel.text = "Is your number " + guess + "?";
+		}
+	}
+
+	public void GuessHiger(){
+		min = guess + 1;
+		NextGuess();
+		ShowGuess();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+		ShowGuess();
+	}
+
+	public void CorrectGuess(){
+		levelManager.LoadLevel("Win");
+	}
+
+
+}
